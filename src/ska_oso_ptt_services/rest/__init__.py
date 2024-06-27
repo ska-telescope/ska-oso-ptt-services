00688997364f1132a0fc0c88e0d7a3898b27904c
Omprakash Verma <omprakash_verma@persistent.com>
2024-06-27 09:39:08 +0530

    NAK-933 Added helm chart support for ptt services

--- src/ska_oso_ptt_services/rest/__init__.py
+++ src/ska_oso_ptt_services/rest/__init__.py
@@ -1,5 +1,3 @@
-"""ska-oso-ptt-services"""
-
 import os
 from importlib.metadata import version
 from typing import Any, Dict
@@ -9,29 +7,13 @@ from connexion import App
 
 KUBE_NAMESPACE = os.getenv("KUBE_NAMESPACE", "ska-oso-ptt-services")
 PTT_MAJOR_VERSION = version("ska-oso-ptt-services").split(".")[0]
-# The base path includes the namespace which is known at runtime
-# to avoid clashes in deployments, for example in CICD
-API_PATH = f"/{KUBE_NAMESPACE}/ptt/api/v{PTT_MAJOR_VERSION}"
+API_PATH = f"{KUBE_NAMESPACE}/ptt/api/v{PTT_MAJOR_VERSION}"
 
 
-def resolve_openapi_spec() -> Dict[str, Any]:
-    """
-    Resolves the $ref in the OpenAPI spec before it is used by Connexion,
-    as Connexion can't parse them.
-    See https://github.com/spec-first/connexion/issues/967
-    """
-    cwd, _ = os.path.split(__file__)
-    path = os.path.join(cwd, "./openapi/ptt-openapi-v1.yaml")
-    parser = prance.ResolvingParser(path, lazy=True, strict=True)
-    parser.parse()
-    return parser.specification
-
-
+# There is a (another) issue with Connexion where it cannot validate
+# against a spec with polymorphism,
+# As a quick has, this basically turns off the validation
 class CustomRequestBodyValidator:
-    """
-    Create the Connexion application with required config
-    """
-
     def __init__(self, *args, **kwargs):
         pass
 
@@ -39,26 +21,39 @@ class CustomRequestBodyValidator:
         return function
 
 
-def create_app(open_api_spec=None):
-    """
-    Create the Connexion application with required config
-    """
+# Resolves the $ref in the OpenAPI spec before it is used by Connexion,
+# as Connexion can't parse them - see https://github.com/spec-first/connexion/issues/967
+def get_openapi_spec() -> Dict[str, Any]:
+    cwd, _ = os.path.split(__file__)
+    path = os.path.join(cwd, "./openapi/ptt-openapi-v1.yaml")
+    parser = prance.ResolvingParser(path, lazy=True, strict=True)
+    parser.parse()
+    spec = parser.specification
+    return spec
 
+
+def init_app(open_api_spec=None):
+    """
+    Initialise the PTT REST application.
+
+    Resolving the spec is relatively time-consuming, so allow a single
+    spec to be injected to all the test app instances to speed up the unit tests
+    """
     if open_api_spec is None:
-        open_api_spec = resolve_openapi_spec()
+        open_api_spec = get_openapi_spec()
 
     validator_map = {
         "body": CustomRequestBodyValidator,
     }
-
     connexion = App(__name__, specification_dir="openapi/")
-
     connexion.add_api(
         open_api_spec,
+        # The base path includes the namespace which is known at runtime
+        # to avoid clashes in deployments, for example in CICD
+        base_path=f"/{API_PATH}",
         arguments={"title": "OpenAPI PTT"},
-        base_path=API_PATH,
-        pythonic_params=True,
         validator_map=validator_map,
+        pythonic_params=True,
     )
 
     app = connexion.app
