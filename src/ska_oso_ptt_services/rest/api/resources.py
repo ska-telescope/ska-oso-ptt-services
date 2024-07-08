8b5a6524fd99262a6f959187074d5396c1b6cf9b
shhaaluu <shalu_mishra@persistent.com>
2024-07-08 11:38:43 +0530

    NAK-938 Created APIs for EB & SBI

--- src/ska_oso_ptt_services/rest/api/resources.py
+++ src/ska_oso_ptt_services/rest/api/resources.py
@@ -17,7 +17,13 @@ from ska_db_oda.rest.api.resources import (
     validation_response,
 )
 from ska_oso_pdm import SBDStatusHistory
-from ska_oso_pdm.entity_status_history import SBDStatus, SBIStatus, SBIStatusHistory
+from ska_oso_pdm.entity_status_history import (
+    OSOEBStatus,
+    OSOEBStatusHistory,
+    SBDStatus,
+    SBIStatus,
+    SBIStatusHistory,
+)
 
 from ska_oso_ptt_services.rest import oda
 
@@ -234,3 +240,241 @@ def put_sbi_history(sbi_id: str, version: int, body: dict) -> Response:
         uow.commit()
 
     return persisted_sbi, HTTPStatus.OK
+
+
+@error_handler
+def get_eb_with_status(eb_id: str) -> Response:
+    """
+    Function that a GET /ebs/<eb_id> request is routed to.
+
+    :param eb_id: Requested identifier from the path parameter
+    :return: The ExecutionBlock with status wrapped in a Response,
+        or appropriate error Response
+    """
+    with oda.uow as uow:
+        eb = uow.ebs.get(eb_id)
+        eb_json = eb.model_dump(mode="json")
+        eb_json["status"] = _get_eb_status(eb_id, eb_json["metadata"]["version"])[
+            "current_status"
+        ]
+
+    return eb_json, HTTPStatus.OK
+
+
+@error_handler
+def get_ebs_with_status(**kwargs) -> Response:
+    """
+    Function that a GET /ebs request is routed to.
+
+    :param kwargs: Parameters to query the ODA by.
+    :return: All ExecutionBlocks present with status wrapped in a Response,
+         or appropriate error Response
+    """
+    maybe_qry_params = get_qry_params(kwargs)
+    if not isinstance(maybe_qry_params, QueryParams):
+        return maybe_qry_params
+
+    with oda.uow as uow:
+        ebs = uow.ebs.query(maybe_qry_params)
+        eb_with_status = [
+            {
+                **eb.model_dump(mode="json"),
+                "status": _get_eb_status(eb.eb_id, eb.metadata.version)[
+                    "current_status"
+                ],
+            }
+            for eb in ebs
+        ]
+    return eb_with_status, HTTPStatus.OK
+
+
+def _get_eb_status(eb_id: str, version: str = None) -> Dict[str, Any]:
+    """
+    Takes an EB ID and Version and returns status
+    :param sbd_id: Execution Block ID
+    :param version: EB version
+
+    Returns retrieved EB status in Dictionary format
+
+    """
+    with oda.uow as uow:
+        retrieved_eb = uow.ebs_status_history.get(
+            entity_id=eb_id, version=version, is_status_history=False
+        )
+
+    return retrieved_eb.model_dump()
+
+
+@error_handler
+def get_eb_status(eb_id: str, version: int = None) -> Response:
+    """
+    Function that a GET status/ebs/<eb_id> request is routed to.
+    This method is used to GET the current status for the given eb_id
+
+    :param eb_id: Requested identifier from the path parameter
+    :param version: Requested identifier from the path parameter
+    :return: The current entity status,OSOEBStatusHistory wrapped in a
+        Response, or appropriate error Response
+    """
+
+    eb_status = _get_eb_status(eb_id=eb_id, version=version)
+    return eb_status, HTTPStatus.OK
+
+
+@error_handler
+def put_eb_history(eb_id: str, version: int, body: dict) -> Response:
+    """
+    Function that a PUT status/ebs/<eb_id> request is routed to.
+
+    :param eb_id: Requested identifier from the path parameter
+    :param version: Requested identifier from the path parameter
+    :param body: ExecutionBlock to persist from the request body
+    :return: The ExecutionBlock wrapped in a Response, or appropriate error Response
+    """
+
+    try:
+        eb_status_history = OSOEBStatusHistory(
+            eb_ref=eb_id,
+            previous_status=OSOEBStatus(body["previous_status"]),
+            current_status=OSOEBStatus(body["current_status"]),
+            metadata={"version": version},
+        )
+
+    except ValueError as err:
+        raise StatusHistoryException(err)  # pylint: disable=W0707
+
+    if response := check_for_mismatch(eb_id, eb_status_history.eb_ref):
+        return response
+
+    with oda.uow as uow:
+        if eb_id not in uow.ebs:
+            raise KeyError(
+                f"Not found. The requested eb_id {eb_id} could not be found."
+            )
+
+        persisted_eb = uow.ebs_status_history.add(eb_status_history)
+        uow.commit()
+
+    return persisted_eb, HTTPStatus.OK
+
+
+@error_handler
+def get_eb_status_history(**kwargs) -> Response:
+    """
+    Function that a GET /status/ebs request is routed to.
+    This method is used to GET status history for the given entity
+
+    :param kwargs: Parameters to query the ODA by.
+    :return: The status history, OSOEBStatusHistory wrapped in a Response,
+        or appropriate error Response
+    """
+    if not isinstance(maybe_qry_params := get_qry_params(kwargs), QueryParams):
+        return maybe_qry_params
+
+    with oda.uow as uow:
+        ebs_status_history = uow.ebs_status_history.query(
+            maybe_qry_params, is_status_history=True
+        )
+        if not ebs_status_history:
+            raise KeyError("not found")
+
+    return ebs_status_history, HTTPStatus.OK
+
+
+@error_handler
+def get_sbi_with_status(sbi_id: str) -> Response:
+    """
+    Function that a GET /sbis/<sbi_id> request is routed to.
+
+    :param sbd_id: Requested identifier from the path parameter
+    :return: The SBDefinition with status wrapped in a Response,
+         or appropriate error Response
+    """
+    with oda.uow as uow:
+        sbi = uow.sbis.get(sbi_id)
+        sbi_json = sbi.model_dump(mode="json")
+        sbi_json["status"] = _get_sbi_status(sbi_id, sbi_json["metadata"]["version"])[
+            "current_status"
+        ]
+    return sbi_json, HTTPStatus.OK
+
+
+@error_handler
+def get_sbis_with_status(**kwargs) -> Response:
+    """
+    Function that a GET /sbis request is routed to.
+
+    :param kwargs: Parameters to query the ODA by.
+    :return: All ExecutionBlocks present with status wrapped in a Response,
+         or appropriate error Response
+    """
+    maybe_qry_params = get_qry_params(kwargs)
+    if not isinstance(maybe_qry_params, QueryParams):
+        return maybe_qry_params
+
+    with oda.uow as uow:
+        sbis = uow.sbis.query(maybe_qry_params)
+        sbi_with_status = [
+            {
+                **sbi.model_dump(mode="json"),
+                "status": _get_sbi_status(sbi.sbi_id, sbi.metadata.version)[
+                    "current_status"
+                ],
+            }
+            for sbi in sbis
+        ]
+    return sbi_with_status, HTTPStatus.OK
+
+
+def _get_sbi_status(sbi_id: str, version: str = None) -> Dict[str, Any]:
+    """
+    Takes an SBInstance ID and Version and returns status
+    param sbd_id: SBInstance ID
+    :param version: SBI version
+
+    Returns retrieved SBI status in Dictionary format
+
+    """
+    with oda.uow as uow:
+        retrieved_sbi = uow.sbis_status_history.get(
+            entity_id=sbi_id, version=version, is_status_history=False
+        )
+    return retrieved_sbi.model_dump()
+
+
+@error_handler
+def get_sbi_status(sbi_id: str, version: int = None) -> Response:
+    """
+    Function that a GET status/sbi/<sbi_id> request is routed to.
+    This method is used to GET the current status for the given sbi_id
+
+    :param sb_id: Requested identifier from the path parameter
+    :param version: Requested identifier from the path parameter
+    :return: The current entity status,SBIStatusHistory wrapped in a
+        Response, or appropriate error Response
+    """
+    sbi_status = _get_sbi_status(sbi_id=sbi_id, version=version)
+    return sbi_status, HTTPStatus.OK
+
+
+@error_handler
+def get_sbi_status_history(**kwargs) -> Response:
+    """
+    Function that a GET /status/history/sbis request is routed to.
+    This method is used to GET status history for the given entity
+
+    :param kwargs: Parameters to query the ODA by.
+    :return: The status history, SBIStatusHistory wrapped in a Response,
+        or appropriate error Response
+    """
+    if not isinstance(maybe_qry_params := get_qry_params(kwargs), QueryParams):
+        return maybe_qry_params
+
+    with oda.uow as uow:
+        sbis_status_history = uow.sbis_status_history.query(
+            maybe_qry_params, is_status_history=True
+        )
+        if not sbis_status_history:
+            raise KeyError("not found")
+
+    return sbis_status_history, HTTPStatus.OK
--- tests/unit/ska_oso_ptt_services/rest/test_resources.py
+++ tests/unit/ska_oso_ptt_services/rest/test_resources.py
@@ -3,8 +3,12 @@ from http import HTTPStatus
 from importlib.metadata import version
 from unittest import mock
 
-from ska_oso_pdm import SBDefinition
-from ska_oso_pdm.entity_status_history import SBDStatusHistory, SBIStatusHistory
+from ska_oso_pdm import OSOExecutionBlock, SBDefinition, SBInstance
+from ska_oso_pdm.entity_status_history import (
+    OSOEBStatusHistory,
+    SBDStatusHistory,
+    SBIStatusHistory,
+)
 
 from tests.unit.ska_oso_ptt_services.util import (
     assert_json_is_equal,
@@ -261,7 +265,188 @@ class TestSBDefinitionAPI:
         assert result.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
 
 
-class TestSBIDefinitionAPI:
+class TestSBInstanceAPI:
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_sbi_status")
+    def test_get_sbis_with_status(self, mock_get_sbi_status, mock_oda, client):
+        valid_sbis = load_string_from_file(
+            "files/testfile_sample_multiple_sbis_with_status.json"
+        )
+
+        sbi_instance = [SBInstance(**x) for x in json.loads(valid_sbis)]
+
+        sbis_mock = mock.MagicMock()
+        sbis_mock.query.return_value = sbi_instance
+        uow_mock = mock.MagicMock()
+        uow_mock.sbis = sbis_mock
+        mock_get_sbi_status.return_value = {"current_status": "created"}
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        # Perform the GET request with query parameters using the query_string parameter
+        query_params = {
+            "match_type": "equals",
+            "created_after": "2022-03-28T15:43:53.971548+00:00",
+        }
+
+        result = client.get(
+            f"{BASE_API_URL}/sbis",
+            query_string=query_params,
+            headers={"accept": "application/json"},
+        )
+
+        assert_json_is_equal(result.text, valid_sbis)
+        assert result.status_code == HTTPStatus.OK
+
+    def test_invalid_get_sbis_with_status(self, client):
+        # Define the query parameters
+        query_params = {
+            "match_type": "equals",
+            "created_before": "2022-03-28T15:43:53.971548+00:00",
+            "created_after": "2022-03-28T15:43:53.971548+00:00",
+            "last_modified_before": "2022-03-28T15:43:53.971548+00:00",
+            "last_modified_after": "2022-03-28T15:43:53.971548+00:00",
+        }
+
+        # Perform the GET request with query parameters using the query_string parameter
+        result = client.get(
+            f"{BASE_API_URL}/sbis",
+            query_string=query_params,
+            headers={"accept": "application/json"},
+        )
+
+        error = {
+            "detail": (
+                "Different query types are not currently supported - for example,"
+                " cannot combine date created query or entity query with a user query"
+            ),
+            "title": "Not Supported",
+        }
+
+        assert json.loads(result.text) == error
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_sbi_status")
+    def test_get_sbi_with_status(self, mock_get_sbi_status, mock_oda, client):
+        valid_sbi = load_string_from_file("files/testfile_sample_sbi_with_status.json")
+
+        sbi_mock = mock.MagicMock()
+        sbi_mock.model_dump.return_value = json.loads(valid_sbi)
+        uow_mock = mock.MagicMock()
+        uow_mock.sbis.get.return_value = sbi_mock
+        mock_get_sbi_status.return_value = {"current_status": "created"}
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        result = client.get(
+            f"{BASE_API_URL}/sbis/sbi-mvp01-20240426-5016",
+            headers={"accept": "application/json"},
+        )
+        assert_json_is_equal(result.text, valid_sbi)
+        assert result.status_code == HTTPStatus.OK
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    def test_get_sbi_with_invalid_status(self, mock_oda, client):
+        invalid_sbi_id = "invalid-sbi-id-12345"
+
+        uow_mock = mock.MagicMock()
+        uow_mock.sbis.get.side_effect = KeyError(
+            f"Not Found. The requested identifier {invalid_sbi_id} could not be found."
+        )
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        # Perform the GET request with the invalid sbi ID
+        result = client.get(
+            f"{BASE_API_URL}/sbis/{invalid_sbi_id}",
+            headers={"accept": "application/json"},
+        )
+
+        # Check if the response contains the expected error message
+        expected_error_message = {
+            "detail": (
+                f"Not Found. The requested identifier {invalid_sbi_id} could not be"
+                " found."
+            )
+        }
+
+        assert result.status_code == HTTPStatus.NOT_FOUND
+        assert result.get_json() == expected_error_message
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    def test_get_sbi_status_history(self, mock_oda, client):
+        valid_sbi_status_history = load_string_from_file(
+            "files/testfile_sample_sbi_status_history.json"
+        )
+
+        uow_mock = mock.MagicMock()
+        uow_mock.sbis_status_history.query.return_value = json.loads(
+            valid_sbi_status_history
+        )
+        mock_oda.uow.__enter__.return_value = uow_mock
+        result = client.get(
+            f"{BASE_API_URL}/status/history/sbis",
+            query_string={"entity_id": "sbi-mvp01-20220923-00002", "version": "1"},
+            headers={"accept": "application/json"},
+        )
+
+        assert_json_is_equal(result.text, valid_sbi_status_history)
+        assert result.status_code == HTTPStatus.OK
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    def test_invalid_get_sbi_status_history(self, mock_oda, client):
+        uow_mock = mock.MagicMock()
+        uow_mock.sbis_status_history.query.return_value = []
+        mock_oda.uow.__enter__.return_value = uow_mock
+        result = client.get(
+            f"{BASE_API_URL}/status/history/sbis",
+            query_string={"entity_id": "sbi-t000-00100", "version": "1"},
+            headers={"accept": "application/json"},
+        )
+
+        error = {
+            "detail": (
+                "Not Found. The requested identifier sbi-t000-00100 could not be found."
+            )
+        }
+        assert json.loads(result.text) == error
+        assert result.status_code == HTTPStatus.NOT_FOUND
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_sbi_status")
+    def test_get_sbi_status(self, mock_get_sbi_status, client):
+        valid_sbi_status = load_string_from_file(
+            "files/testfile_sample_sbi_status.json"
+        )
+        mock_get_sbi_status.return_value = json.loads(valid_sbi_status)
+
+        result = client.get(
+            f"{BASE_API_URL}/status/sbis/sbi-mvp01-20240426-5016",
+            query_string={"version": "1"},
+            headers={"accept": "application/json"},
+        )
+
+        assert_json_is_equal(result.text, valid_sbi_status)
+        assert result.status_code == HTTPStatus.OK
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_sbi_status")
+    def test_invalid_get_sbi_status(self, mock_get_sbi_status, client):
+        invalid_sbi_id = "sbi-t0001-20240702-00100"
+        mock_get_sbi_status.side_effect = KeyError(
+            f"Not Found. The requested identifier {invalid_sbi_id} could not be found."
+        )
+
+        result = client.get(
+            f"{BASE_API_URL}/status/sbis/{invalid_sbi_id}",
+            query_string={"version": "1"},
+            headers={"accept": "application/json"},
+        )
+
+        error = {
+            "detail": (
+                "Not Found. The requested identifier sbi-t0001-20240702-00100 could not"
+                " be found."
+            )
+        }
+        assert json.loads(result.text) == error
+        assert result.status_code == HTTPStatus.NOT_FOUND
+
     @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
     def test_put_sbi_history(self, mock_oda, client):
         valid_put_sbi_history_response = load_string_from_file(
@@ -312,6 +497,252 @@ class TestSBIDefinitionAPI:
             json=data,
             headers={"accept": "application/json"},
         )
+        exclude_path = ["root['traceback']"]
+        assert_json_is_equal(result.text, json.dumps(error), exclude_path)
+        assert result.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
+
+
+class TestExecutionBlockAPI:
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_eb_status")
+    def test_get_ebs_with_status(self, mock_get_eb_status, mock_oda, client):
+        valid_ebs = load_string_from_file(
+            "files/testfile_sample_multiple_ebs_with_status.json"
+        )
+
+        execution_block = [OSOExecutionBlock(**x) for x in json.loads(valid_ebs)]
+
+        ebs_mock = mock.MagicMock()
+        ebs_mock.query.return_value = execution_block
+        uow_mock = mock.MagicMock()
+        uow_mock.ebs = ebs_mock
+        mock_get_eb_status.return_value = {"current_status": "fully_observed"}
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        # Perform the GET request with query parameters using the query_string parameter
+        query_params = {
+            "match_type": "equals",
+            "created_after": "2022-03-28T15:43:53.971548+00:00",
+        }
+
+        result = client.get(
+            f"{BASE_API_URL}/ebs",
+            query_string=query_params,
+            headers={"accept": "application/json"},
+        )
+
+        assert_json_is_equal(result.text, valid_ebs)
+        assert result.status_code == HTTPStatus.OK
+
+    def test_invalid_get_ebs_with_status(self, client):
+        # Define the query parameters
+        query_params = {
+            "match_type": "equals",
+            "created_before": "2022-03-28T15:43:53.971548+00:00",
+            "created_after": "2022-03-28T15:43:53.971548+00:00",
+            "last_modified_before": "2022-03-28T15:43:53.971548+00:00",
+            "last_modified_after": "2022-03-28T15:43:53.971548+00:00",
+        }
+
+        # Perform the GET request with query parameters using the query_string parameter
+        result = client.get(
+            f"{BASE_API_URL}/ebs",
+            query_string=query_params,
+            headers={"accept": "application/json"},
+        )
+
+        error = {
+            "detail": (
+                "Different query types are not currently supported - for example,"
+                " cannot combine date created query or entity query with a user query"
+            ),
+            "title": "Not Supported",
+        }
+
+        assert json.loads(result.text) == error
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_eb_status")
+    def test_get_eb_with_status(self, mock_get_eb_status, mock_oda, client):
+        valid_eb_with_status = load_string_from_file(
+            "files/testfile_sample_eb_with_status.json"
+        )
+
+        eb_mock = mock.MagicMock()
+        eb_mock.model_dump.return_value = json.loads(valid_eb_with_status)
+        uow_mock = mock.MagicMock()
+        uow_mock.ebs.get.return_value = eb_mock
+        mock_get_eb_status.return_value = {"current_status": "fully_observed"}
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        result = client.get(
+            f"{BASE_API_URL}/ebs/eb-mvp01-20240426-5004",
+            headers={"accept": "application/json"},
+        )
+        assert_json_is_equal(result.text, valid_eb_with_status)
+        assert result.status_code == HTTPStatus.OK
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    def test_get_eb_with_invalid_status(self, mock_oda, client):
+        invalid_eb_id = "invalid-eb-id-12345"
+
+        uow_mock = mock.MagicMock()
+        uow_mock.ebs.get.side_effect = KeyError(
+            f"Not Found. The requested identifier {invalid_eb_id} could not be found."
+        )
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        # Perform the GET request with the invalid eb ID
+        result = client.get(
+            f"{BASE_API_URL}/ebs/{invalid_eb_id}",
+            headers={"accept": "application/json"},
+        )
+
+        # Check if the response contains the expected error message
+        expected_error_message = {
+            "detail": (
+                f"Not Found. The requested identifier {invalid_eb_id} could not be"
+                " found."
+            )
+        }
+
+        assert result.status_code == HTTPStatus.NOT_FOUND
+        assert result.get_json() == expected_error_message
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    def test_get_eb_status_history(self, mock_oda, client):
+        """
+        uow_mock = mock.MagicMock()
+        uow_mock.ebs.get.return_value = TestDataFactory.ebefinition() #uow.ebs.get
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        result = client.get(f"{ebS_API_URL}/eb-1234")
+        """
+        valid_eb_status_history = load_string_from_file(
+            "files/testfile_sample_eb_status_history.json"
+        )
+
+        uow_mock = mock.MagicMock()
+        uow_mock.ebs_status_history.query.return_value = json.loads(
+            valid_eb_status_history
+        )
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        result = client.get(
+            f"{BASE_API_URL}/status/history/ebs",
+            query_string={"entity_id": "eb-mvp01-20240426-5004", "version": "1"},
+            headers={"accept": "application/json"},
+        )
+        assert_json_is_equal(result.text, valid_eb_status_history)
+
+        assert result.status_code == HTTPStatus.OK
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    def test_invalid_get_eb_status_history(self, mock_oda, client):
+        uow_mock = mock.MagicMock()
+        uow_mock.ebs_status_history.query.return_value = []
+        mock_oda.uow.__enter__.return_value = uow_mock
+        result = client.get(
+            f"{BASE_API_URL}/status/history/ebs",
+            query_string={"entity_id": "eb-t0001-00100", "version": "1"},
+            headers={"accept": "application/json"},
+        )
+
+        error = {
+            "detail": (
+                "Not Found. The requested identifier eb-t0001-00100 could not be found."
+            )
+        }
+        assert json.loads(result.text) == error
+        assert result.status_code == HTTPStatus.NOT_FOUND
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_eb_status")
+    def test_get_eb_status(self, mock_get_eb_status, client):
+        valid_eb_status = load_string_from_file("files/testfile_sample_eb_status.json")
+        mock_get_eb_status.return_value = json.loads(valid_eb_status)
+
+        result = client.get(
+            f"{BASE_API_URL}/status/ebs/eb-mvp01-20240426-5004",
+            query_string={"version": "1"},
+            headers={"accept": "application/json"},
+        )
+
+        assert_json_is_equal(result.text, valid_eb_status)
+        assert result.status_code == HTTPStatus.OK
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources._get_eb_status")
+    def test_invalid_get_eb_status(self, mock_get_eb_status, client):
+        invalid_eb_id = "eb-t0001-20240702-00100"
+        mock_get_eb_status.side_effect = KeyError(
+            f"Not Found. The requested identifier {invalid_eb_id} could not be found."
+        )
+
+        result = client.get(
+            f"{BASE_API_URL}/status/ebs/{invalid_eb_id}",
+            query_string={"version": "1"},
+            headers={"accept": "application/json"},
+        )
+
+        error = {
+            "detail": (
+                "Not Found. The requested identifier eb-t0001-20240702-00100 could not"
+                " be found."
+            )
+        }
+        assert json.loads(result.text) == error
+        assert result.status_code == HTTPStatus.NOT_FOUND
+
+    @mock.patch("ska_oso_ptt_services.rest.api.resources.oda")
+    def test_put_eb_history(self, mock_oda, client):
+        valid_put_eb_history_response = load_string_from_file(
+            "files/testfile_sample_eb_status.json"
+        )
+
+        uow_mock = mock.MagicMock()
+        uow_mock.ebs = ["eb-mvp01-20240426-5004"]
+
+        ebs_status_history_mock = mock.MagicMock()
+        ebs_status_history_mock.add.return_value = (
+            OSOEBStatusHistory.model_validate_json(valid_put_eb_history_response)
+        )
+
+        uow_mock.ebs_status_history = ebs_status_history_mock
+        uow_mock.commit.return_value = "200"
+        mock_oda.uow.__enter__.return_value = uow_mock
+
+        url = "/ska-oso-ptt-services/ptt/api/v1/status/ebs/eb-mvp01-20240426-5004"
+        query_params = {"version": "1"}
+        data = {"current_status": "fully_observed", "previous_status": "created"}
+        exclude_paths = [
+            "root['metadata']['created_on']",
+            "root['metadata']['last_modified_on']",
+        ]
+
+        result = client.put(
+            url,
+            query_string=query_params,
+            json=data,
+            headers={"accept": "application/json"},
+        )
+        assert_json_is_equal(result.text, valid_put_eb_history_response, exclude_paths)
+        assert result.status_code == HTTPStatus.OK
+
+    def test_invalid_put_eb_history(self, client):
+        query_params = {"version": "1"}
+        data = {"current1_status": "fully_observed", "previous_status": "created"}
+
+        error = {
+            "detail": "KeyError('current_status') with args ('current_status',)",
+            "title": "Internal Server Error",
+        }
+
+        result = client.put(
+            f"{BASE_API_URL}/status/ebs/eb-t0001-20240702-00002",
+            query_string=query_params,
+            json=data,
+            headers={"accept": "application/json"},
+        )
+
         exclude_path = ["root['traceback']"]
         assert_json_is_equal(result.text, json.dumps(error), exclude_path)
         assert result.status_code == HTTPStatus.INTERNAL_SERVER_ERROR
