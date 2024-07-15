b97286ed0c15478d9e3996c139c6d8cefdfa1978
Betelgeusenova <sagar_wankhede1@persistent.com>
2024-07-15 19:23:16 +0530

    NAK-986 Added project status GET, PUT APIs

--- src/ska_oso_ptt_services/rest/api/resources.py
+++ src/ska_oso_ptt_services/rest/api/resources.py
@@ -4,7 +4,6 @@ Functions which the HTTP requests to individual resources are mapped to.
 See the operationId fields of the Open API spec for the specific mappings.
 """
 
-# pylint: disable=broad-except
 import logging
 from http import HTTPStatus
 from typing import Any, Dict, Tuple, Union
@@ -21,6 +20,8 @@ from ska_oso_pdm import SBDStatusHistory
 from ska_oso_pdm.entity_status_history import (
     OSOEBStatus,
     OSOEBStatusHistory,
+    ProjectStatus,
+    ProjectStatusHistory,
     SBDStatus,
     SBIStatus,
     SBIStatusHistory,
@@ -42,7 +43,7 @@ class PTTQueryParamsFactory(QueryParamsFactory):
     @staticmethod
     def from_dict(kwargs: dict) -> QueryParams:
         """
-        Returns QueryParams instance if validation successfull
+        Returns QueryParams instance if validation successful
         param kwargs: Parameters Passed
         raises: ValueError for incorrect values
         """
@@ -489,3 +490,145 @@ def get_sbi_status_history(**kwargs) -> Response:
             raise KeyError("not found")
 
     return sbis_status_history, HTTPStatus.OK
+
+
+@error_handler
+def get_prj_with_status(prj_id: str) -> Response:
+    """
+    Function that a GET /prjs/<prj_id> request is routed to.
+
+    :param prj_id: Requested identifier from the path parameter
+    :return: The Project with status wrapped in a Response,
+        or appropriate error Response
+    """
+    with oda.uow as uow:
+        prj = uow.prjs.get(prj_id)
+        prj_json = prj.model_dump(mode="json")
+        prj_json["status"] = _get_prj_status(
+            uow=uow, prj_id=prj_id, version=prj_json["metadata"]["version"]
+        )["current_status"]
+
+    return prj_json, HTTPStatus.OK
+
+
+@error_handler
+def get_prjs_with_status(**kwargs) -> Response:
+    """
+    Function that a GET /prjs request is routed to.
+
+    :param kwargs: Parameters to query the ODA by.
+    :return: All Project present with status wrapped in a Response,
+         or appropriate error Response
+    """
+    maybe_qry_params = get_qry_params(kwargs)
+    if not isinstance(maybe_qry_params, QueryParams):
+        return maybe_qry_params
+
+    with oda.uow as uow:
+        prjs = uow.prjs.query(maybe_qry_params)
+        prj_with_status = [
+            {
+                **prj.model_dump(mode="json"),
+                "status": _get_prj_status(
+                    uow=uow, prj_id=prj.prj_id, version=prj.metadata.version
+                )["current_status"],
+            }
+            for prj in prjs
+        ]
+    return prj_with_status, HTTPStatus.OK
+
+
+def _get_prj_status(
+    uow: PostgresUnitOfWork, prj_id: str, version: str = None
+) -> Dict[str, Any]:
+    """
+    Takes an Project ID and Version and returns status
+    :param: uow: ODA PostgresUnitOfWork
+    :param prj_id: project ID
+    :param version: project version
+
+    Returns retrieved project status in Dictionary format
+
+    """
+
+    retrieved_prj = uow.prjs_status_history.get(
+        entity_id=prj_id, version=version, is_status_history=False
+    )
+
+    return retrieved_prj.model_dump()
+
+
+@error_handler
+def get_prj_status(prj_id: str, version: int = None) -> Response:
+    """
+    Function that a GET status/prjs/<prj_id> request is routed to.
+    This method is used to GET the current status for the given prj_id
+
+    :param prj_id: Requested identifier from the path parameter
+    :param version: Requested identifier from the path parameter
+    :return: The current entity status,ProjectStatusHistory wrapped in a
+        Response, or appropriate error Response
+    """
+    with oda.uow as uow:
+        prj_status = _get_prj_status(uow=uow, prj_id=prj_id, version=version)
+    return prj_status, HTTPStatus.OK
+
+
+@error_handler
+def put_prj_history(prj_id: str, version: int, body: dict) -> Response:
+    """
+    Function that a PUT status/prjs/<prj_id> request is routed to.
+
+    :param prj_id: Requested identifier from the path parameter
+    :param version: Requested identifier from the path parameter
+    :param body: Project to persist from the request body
+    :return: The Project wrapped in a Response, or appropriate error Response
+    """
+
+    try:
+        prj_status_history = ProjectStatusHistory(
+            prj_ref=prj_id,
+            previous_status=ProjectStatus(body["previous_status"]),
+            current_status=ProjectStatus(body["current_status"]),
+            metadata={"version": version},
+        )
+
+    except ValueError as err:
+        raise StatusHistoryException(err)  # pylint: disable=W0707
+
+    if response := check_for_mismatch(prj_id, prj_status_history.prj_ref):
+        return response
+
+    with oda.uow as uow:
+        if prj_id not in uow.prjs:
+            raise KeyError(
+                f"Not found. The requested prj_id {prj_id} could not be found."
+            )
+
+        persisted_prj = uow.prjs_status_history.add(prj_status_history)
+        uow.commit()
+
+    return persisted_prj, HTTPStatus.OK
+
+
+@error_handler
+def get_prj_status_history(**kwargs) -> Response:
+    """
+    Function that a GET /status/prjs request is routed to.
+    This method is used to GET status history for the given entity
+
+    :param kwargs: Parameters to query the ODA by.
+    :return: The status history, ProjectStatusHistory wrapped in a Response,
+        or appropriate error Response
+    """
+    if not isinstance(maybe_qry_params := get_qry_params(kwargs), QueryParams):
+        return maybe_qry_params
+
+    with oda.uow as uow:
+        prjs_status_history = uow.prjs_status_history.query(
+            maybe_qry_params, is_status_history=True
+        )
+        if not prjs_status_history:
+            raise KeyError("not found")
+
+    return prjs_status_history, HTTPStatus.OK
