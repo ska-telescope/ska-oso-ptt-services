3d959f48365c7a6894a85e1f86c6dba69cfa8a6a
Omprakash Verma <omprakash_verma@persistent.com>
2024-07-18 17:15:23 +0530

    NAK-944 Updated values file

--- src/ska_oso_ptt_services/rest/__init__.py
+++ src/ska_oso_ptt_services/rest/__init__.py
@@ -43,13 +43,6 @@ class CustomRequestBodyValidator:
         return function
 
 
-def set_default_headers_on_response(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "*"
-    return response
-
-
 def create_app(open_api_spec=None) -> App:
     """
     Create the Connexion application with required config
@@ -61,8 +54,12 @@ def create_app(open_api_spec=None) -> App:
     connexion = App(__name__, specification_dir="openapi/")
 
     connexion.app.json_encoder = PdmJsonEncoder
-    # Used for local deployment
-    # connexion.app.config.from_object("ska_oso_ptt_services.rest.config.Config")
+
+    def set_default_headers_on_response(response):
+        response.headers["Access-Control-Allow-Origin"] = "*"
+        response.headers["Access-Control-Allow-Headers"] = "*"
+        response.headers["Access-Control-Allow-Methods"] = "*"
+        return response
 
     connexion.app.after_request(set_default_headers_on_response)
 
@@ -76,6 +73,7 @@ def create_app(open_api_spec=None) -> App:
         pythonic_params=True,
         validator_map=validator_map,
     )
+
     oda.init_app(connexion.app)
 
     return connexion
